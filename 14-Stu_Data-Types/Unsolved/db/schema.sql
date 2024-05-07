8bd19ea3126db351c16402066f0b2019dcfae7e4
Josie Zobel <159351625+jobeans24@users.noreply.github.com>
2024-05-07 14:12:05 -0400

    importing class files from may 6

--- 14-Stu_Data-Types/Unsolved/db/schema.sql
+++ 14-Stu_Data-Types/Unsolved/db/schema.sql
@@ -1,11 +1,11 @@
 DROP DATABASE IF EXISTS customer_db;
 CREATE DATABASE customer_db;
 
-USE customer_db;
+\c customer_db;
 
 CREATE TABLE customers (
-  id,
-  first_name,
-  last_name,
-  value_card_member
+  id SERIAL PRIMARY KEY NOT NULL,
+  first_name VARCHAR(100) NOT NULL,
+  last_name VARCHAR(100) NOT NULL,
+  value_card_member BOOLEAN DEFAULT FALSE
 );
--- 16-Stu_Schema/Unsolved/db/schema.sql
+++ 16-Stu_Schema/Unsolved/db/schema.sql
@@ -1,1 +1,21 @@
 -- Write your schema here --
+DROP DATABASE IF EXISTS amazon_db;
+CREATE DATABASE amazon_db;
+
+\c amazon_db;
+
+CREATE TABLE products (
+    id SERIAL PRIMARY KEY,
+    product_name VARCHAR(255) NOT NULL,
+    department_name VARCHAR(255) NOT NULL,
+    price DECIMAL(10, 2) NOT NULL,
+);
+
+CREATE TABLE orders (
+    id SERIAL PRIMARY KEY,
+    product_id INT NOT NULL,
+   
+    FOREIGN KEY (product_id) REFERENCES products(id)
+    ON DELETE SET NULL
+);
+
